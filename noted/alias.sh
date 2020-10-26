c0e8c84084022036af546c0edfeab5e0b795146a
axxander <axander.gregory@gmail.com>
2020-10-26 13:40:12 +0000

    Updated shell scipt for creating noted alias.

--- noted/alias.sh
+++ noted/alias.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# Adds "noted" alias to shell
+echo " " >> ~/.bash_profile
+echo "# Alias for Noted application" >> ~/.bash_profile
 echo "alias noted=\"python3 $(pwd)/noted.py\"" >> ~/.bash_profile
